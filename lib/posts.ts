2494c2f9f908782795d26814abddd626fb0190da
RunningLearner <tkacnsdms@naver.com>
2022-12-06 11:50:26 +0900

    fix: 오타수정

--- lib/posts.ts
+++ lib/posts.ts
@@ -17,7 +17,7 @@ export function getSortedPostData() {
 
     return {
       id,
-      ...allPostsData(matterResult.data as { date: string; title: string }),
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
 
